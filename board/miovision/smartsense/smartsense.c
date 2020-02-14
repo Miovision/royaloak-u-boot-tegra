273e56e04d84c1ead5f5e19e054ec43bab3a623c
Mike Loder <mloder@miovision.com>
2020-02-14 17:08:10 -0500

    u-boot configures LM96063 (LM63) for tach
    *Ported from Smart Sense Repo*
    
    u-boot configures LM96063 (LM63) for tach
    
    The LM96063 power on default is the tach function is disabled. The
    current linux kernel driver has no facility for enabling or disabling
    this at all; it only checks to see if the tach function is enabled. In
    fact, comments in the driver code say it's expecting the BIOS to have
    set up the LM63-compatible device.
    
    Enable the tach function in the chip. This causes the driver to add the
    fan1_* device entries, which allows monitoring of the tach.
    
    NOTE: i2c bus numbering is differeing in u-boot than in the linux
    kernel. I do not know why.

--- board/miovision/smartsense/smartsense.c
+++ board/miovision/smartsense/smartsense.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016, NVIDIA CORPORATION
+ * Copyright (c) 2020, Miovision
  *
  * SPDX-License-Identifier: GPL-2.0
  */
@@ -13,43 +14,41 @@ int tegra_board_init(void)
 	struct udevice *dev;
 	uchar val;
 	int ret;
+	int err = 0;
+
+	/*
+	   NOTE: I2C bus numbering is not the same here as it is for the kernel;
+	   Bus 1 here is i2c-0 for the kernel.
+    */
 
 	/* Turn on MAX77620 LDO3 to 3.3V for SD card power */
 	debug("%s: Set LDO3 for VDDIO_SDMMC_AP power to 3.3V\n", __func__);
 	ret = i2c_get_chip_for_busnum(0, MAX77620_I2C_ADDR_7BIT, 1, &dev);
 	if (ret) {
 		printf("%s: Cannot find MAX77620 I2C chip\n", __func__);
-		return ret;
-	}
-	/* 0xF2 for 3.3v, enabled: bit7:6 = 11 = enable, bit5:0 = voltage */
-	val = 0xF2;
-	ret = dm_i2c_write(dev, MAX77620_CNFG1_L3_REG, &val, 1);
-	if (ret) {
-		printf("i2c_write 0 0x3c 0x27 failed: %d\n", ret);
-		return ret;
+		err = ret;
+	} else {
+		/* 0xF2 for 3.3v, enabled: bit7:6 = 11 = enable, bit5:0 = voltage */
+		val = 0xF2;
+		ret = dm_i2c_write(dev, MAX77620_CNFG1_L3_REG, &val, 1);
+		if (ret) {
+			printf("i2c_write 0 0x3c 0x27 failed: %d\n", ret);
+			err = ret;
+		}
 	}
 
-	return 0;
+	ret = i2c_get_chip_for_busnum(1, 0x4c, 1, &dev);
+	if (ret) {
+		printf("%s: Cannot find LM96063 I2C chip\n", __func__);
+	} else {
+		/* Turn on the tachometer function, it's off by default */
+		val = 0x04;
+		ret = dm_i2c_write(dev, 0x03, &val, 1);
+		if (ret) {
+			printf("i2c_write 0 0x4c 0x03 failed: %d\n", ret);
+		}
+	}
+
+	return err;
 }
 
-int tegra_pcie_board_init(void)
-{
-	struct udevice *dev;
-	uchar val;
-	int ret;
-
-	/* Turn on MAX77620 LDO7 to 1.05V for PEX power */
-	debug("%s: Set LDO7 for PEX power to 1.05V\n", __func__);
-	ret = i2c_get_chip_for_busnum(0, MAX77620_I2C_ADDR_7BIT, 1, &dev);
-	if (ret) {
-		printf("%s: Cannot find MAX77620 I2C chip\n", __func__);
-		return -1;
-	}
-	/* 0xC5 for 1.05v, enabled: bit7:6 = 11 = enable, bit5:0 = voltage */
-	val = 0xC5;
-	ret = dm_i2c_write(dev, MAX77620_CNFG1_L7_REG, &val, 1);
-	if (ret)
-		printf("i2c_write 0 0x3c 0x31 failed: %d\n", ret);
-
-	return 0;
-}
